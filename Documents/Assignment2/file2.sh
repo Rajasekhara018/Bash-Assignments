de13e30f950cf11ecaa869beb4939353b72d7390
Rajashekar <rajshekarbadam001@gmail.com>
2022-06-14 12:51:56 +0530

    first commit

---
+++ Documents/Assignment2/file2.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+echo "Enter first name:"
+read firstname
+if [[ "$firstname" =~ [^a-zA-Z] ]]
+then
+    echo "Enter the alphabets"
+else
+    echo "valid"
+fi
+echo "Enter lastname:"
+read lastname
+if [[ "$lastname" =~ [^a-zA-Z] ]]
+then
+    echo "inValid"
+else
+    echo "valid"
+fi
+   echo "$firstname.$lastname"
+   echo "$firstname.$lastname"|sha1sum
